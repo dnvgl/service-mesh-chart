68f9bfa57acf41babdc815a5e4ccbdee18c29b34
Aaron Reicher <aaron.reicher@dnv.com>
2022-02-11 15:08:57 -0800

    add testing script and results output
    tests should now be run at each commit and results compared manually

---
+++ testing/generate-results.sh
@@ -0,0 +1,64 @@
+rm results/*.yaml
+echo `date` > results/run-date.txt
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+     > results/base-case.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set gateway.exposeService=false \
+    > results/not-exposed.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set deploymentOnly=true \
+    > results/deployment-only.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.enabled=false \
+    > results/vs-default-routing-disabled.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.retries.enabled=true \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-with-retries.yaml
+
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.allHosts=true \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-all-hosts.yaml
+
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.urlPrefixes= \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-no-urlPrefixes.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.rewriteUrlPrefix.enabled=false \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-rewriteUrlPrefix-disabled.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.redirectOnNoTrailingSlash=false \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-no-slash-redirect.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    -f cors-policy-values.yaml \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-cors-policy.yaml
+
+helm template test-release ../charts/platform-service -n test-ns -f values.yaml \
+    --set defaultRouting.urlExactMatches[0]="url1",defaultRouting.urlExactMatches[0]="url2" \
+    --show-only templates/virtualservice.yaml \
+    > results/vs-exact-matches.yaml
+
+
+echo " *** kubeval results ***"
+kubeval --ignore-missing-schemas results/*.yaml
+echo " *** istioctl validation results ***"
+for f in $(ls results/*.yaml);
+do
+  echo istioctl validating $f;
+  cat $f | istioctl validate -f -
+done;
